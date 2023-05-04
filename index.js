073a3439762d5a665d64103e962904ccd869bc54
sudipchakraborty123456 <sudipchakraborty47@gmail.com>
2023-05-04 11:54:53 +0530

    ff

--- index.js
+++ index.js
@@ -53,30 +53,36 @@ app.get('/callback', async (req, res) => {
 app.get('/sendmail', async (req, res) => {
   const {authToken,fromAddress,toAddress,subject,content,accountId}= req
  
+let data = JSON.stringify({
+  "fromAddress": fromAddress,
+  "toAddress": toAddress,
+  "ccAddress": "",
+  "bccAddress": "",
+  "subject":subject,
+  "content":content,
+  "askReceipt": "yes"
+});
 
-  try {
-    const response = await axios.post(
-      `https://mail.zoho.in/api/accounts/${accountId}/messages`,
-      {
-        fromAddress,
-        toAddress,
-        subject,
-        content,
-        "askReceipt" : "yes"
-      },
-      {
-        headers: {
-          Authorization: `Zoho-oauthtoken ${authToken}`,
-          'Content-Type': 'application/json'
-        }
-      }
-    );
-    console.log(response.data);
-    res.send({data:response.data})
-  } catch (error) {
-    console.error(error.response.data);
-    res.send({data:error.response.data})
-  }
+let config = {
+  method: 'post',
+  maxBodyLength: Infinity,
+  url: `https://mail.zoho.in/api/accounts/${accountId}/messages`,
+  headers: { 
+    'Authorization': `Zoho-oauthtoken ${authToken}`, 
+    'Content-Type': 'application/json', 
+    'Cookie': 'zmuac=NjAwMjEwMzIzNTZfMjQ2MDA2OQ==; stk=847086a69e85b68e7a48adc1c850a9a0; 41d1edd07e=d301060b6da68a115fd1c075cd3651c1; JSESSIONID=F46C0FA89D9DAEE459D5117AAC7D6DD0; _zcsr_tmp=7e74112f-21e7-49e8-9b38-daa451113221; zmcsr=7e74112f-21e7-49e8-9b38-daa451113221'
+  },
+  data : data
+};
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
+
 });
 app.listen(3000, () => {
   console.log('Server started on port 3000');
